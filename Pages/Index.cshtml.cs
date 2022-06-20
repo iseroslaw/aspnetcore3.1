7acb94b0137feacf2c860cdd101e33845243d829
iseroslaw <gabriele.seroni@outlook.it>
2022-06-20 14:09:06 +0200

    get to azure function

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,9 +18,18 @@ namespace aspnetcore3._1.Pages
             _logger = logger;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(SearchString))
+                return;
 
+            using var httpclient = new HttpClient();
+            using var response = httpclient.GetAsync($"https://gasesandboxfunction.azurewebsites.net/api/HttpTrigger1?name={SearchString}").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            ViewData["Message"] = content;
         }
     }
 }
